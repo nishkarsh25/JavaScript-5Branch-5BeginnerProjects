5ddacad5d993b757160d06d111d6b9423b5d0c3c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-29 11:00:32 +0530

    Event Listeners:
    btn: Listens for click events and calls the guessNumber function.
    input: Listens for keydown events, specifically the "Enter" key, and calls the guessNumber function.

--- src/script.js
+++ src/script.js
@@ -5,3 +5,14 @@ let guesses = document.getElementById("guesses");
 
 let answer = Math.floor(Math.random() * 100) + 1;
 let numGuesses = 0;
+
+btn.addEventListener("click", () => {
+    guessNumber();
+  });
+  
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      guessNumber();
+    }
+  });
